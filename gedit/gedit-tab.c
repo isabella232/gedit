7bb880114e066da72ac7dbc2c1e63995fe1805e4
Sébastien Wilmet <swilmet@gnome.org>
2020-03-24 10:51:43 +0100

    io-error-info-bar: tepl function renamed to have a shorter name
    tepl_io_error_info_bar_file_already_open_warning_new() was added very
    recently in tepl git master and has been renamed.

--- gedit/gedit-tab.c
+++ gedit/gedit-tab.c
@@ -1753,7 +1753,7 @@ successful_load (GTask *loading_task)
 
 		set_editable (data->tab, FALSE);
 
-		info_bar = tepl_io_error_info_bar_file_already_open_warning_new (location);
+		info_bar = tepl_io_error_info_bar_file_already_open (location);
 		tepl_info_bar_set_buttons_orientation (info_bar, GTK_ORIENTATION_HORIZONTAL);
 
 		g_signal_connect (info_bar,
